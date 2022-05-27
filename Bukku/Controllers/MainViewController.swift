6663f4c5030dd46101a478fe32d7726e60481ce9
eung7 <wannabeeung@gmail.com>
2022-05-28 01:10:46 +0900

    [Add] Calendar, DetailView

--- Bukku/Controllers/MainViewController.swift
+++ Bukku/Controllers/MainViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 import FSCalendar
 
 class MainViewController: UIViewController {
+    let detailView = DetailView()
+    
     let mainLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 38.0, weight: .heavy)
@@ -35,9 +37,13 @@ class MainViewController: UIViewController {
         return view
     }()
     
-    let goalButton: UIButton = {
-        let button = UIButton()
+    let changeGoalButton: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName: "circle.dashed.inset.filled")
+        config.baseForegroundColor = .systemBackground
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration.init(pointSize: 24)
         
+        let button = UIButton(configuration: config)
         return button
     }()
     
@@ -57,10 +63,9 @@ class MainViewController: UIViewController {
         calendar.appearance.weekdayTextColor = UIColor.init(rgb: 0xAEBD77)
         calendar.appearance.titleDefaultColor = UIColor.init(rgb: 0xE0E7F1)
         calendar.appearance.todayColor = UIColor.init(rgb: 0xAEBD77)
-        calendar.appearance.selectionColor = nil
+        calendar.appearance.selectionColor = UIColor.init(rgb: 0x1B4B36)
         calendar.appearance.weekdayFont = .systemFont(ofSize: 20.0, weight: .thin)
         calendar.appearance.titleFont = .systemFont(ofSize: 18, weight: .thin)
-        
         
         return calendar
     }()
@@ -72,8 +77,9 @@ class MainViewController: UIViewController {
     
     func setupUI() {
         view.backgroundColor = UIColor.init(rgb: 0x538F6A)
+        detailView.alpha = 0
         
-        [ mainLabel, goalLabel, lineView, calendar ]
+        [ mainLabel, goalLabel, changeGoalButton, lineView, calendar, detailView ]
             .forEach { view.addSubview($0) }
         
         mainLabel.snp.makeConstraints { make in
@@ -83,6 +89,11 @@ class MainViewController: UIViewController {
         goalLabel.snp.makeConstraints { make in
             make.top.equalTo(mainLabel.snp.bottom).offset(4)
             make.leading.equalToSuperview().inset(20)
+            make.trailing.equalTo(changeGoalButton.snp.leading).offset(4)
+        }
+        
+        changeGoalButton.snp.makeConstraints { make in
+            make.trailing.top.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
         lineView.snp.makeConstraints { make in
@@ -94,11 +105,28 @@ class MainViewController: UIViewController {
         calendar.snp.makeConstraints { make in
             make.top.equalTo(lineView.snp.bottom).offset(16.0)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(400)
+            make.bottom.equalTo(detailView.snp.top)
+        }
+        
+        detailView.snp.makeConstraints { make in
+            make.height.equalTo(220)
+            make.trailing.leading.equalToSuperview().inset(8)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(8)
         }
     }
 }
 
 extension MainViewController: FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {
+            self.detailView.alpha = 1
+        }, completion: nil)
+        detailView.label.text = date.formatted()
+    }
     
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {
+            self.detailView.alpha = 0
+        }, completion: nil)
+    }
 }
