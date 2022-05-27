acbca9e2fed5abe623d649f7b8308137547437de
eung7 <wannabeeung@gmail.com>
2022-05-27 21:09:38 +0900

    First Commit

---
+++ Minimum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minimum
+//
+//  Created by 김응철 on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
